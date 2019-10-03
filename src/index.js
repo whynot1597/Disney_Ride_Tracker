192e73c0fc87dcb5173111f3edc3aadfbe0ccb19
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-03 16:56:12 -0700

    remove font import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import 'typeface-roboto'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
