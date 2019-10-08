f6c1290e8d8e5633600deb87746be0a52f05d11c
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-08 11:02:51 -0700

    create all components

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCLsfpFpk2e2V-eDHuGfBbkAFuKHjzlJZI",
@@ -14,6 +15,22 @@ const firebaseConfig = {
   class Firebase {
     constructor() {
       app.initializeApp(firebaseConfig);
+
+      this.auth = app.auth();
     }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+      this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    
+    doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
   }
   export default Firebase;
