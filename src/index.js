56862e4fb7a4071e3f2be0edfd8b8dc9845c95eb
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-10 11:07:33 -0700

    revert all Firebase additions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker'
-import App from './components/App';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import App from './App';
 
-ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <App />
-  </FirebaseContext.Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker()
