24359f881718791c148879bebb4dec08fd785d81
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-17 00:00:54 -0700

    Add Google signin

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -11,6 +11,7 @@ const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <SignInGoogle />
     <PasswordForgetLink />
     <SignUpLink />
   </div>
@@ -20,6 +21,7 @@ const INITIAL_STATE = {
   password: '',
   error: null,
 };
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
@@ -68,9 +70,54 @@ class SignInFormBase extends Component {
     );
   }
 }
+
+class SignInGoogleBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  onSubmit = event => {
+    this.props.firebase
+      .doSignInWithGoogle()
+      .then(socialAuthUser => {
+        // Create a user in your Firebase Realtime Database too
+        return this.props.firebase
+          .user(socialAuthUser.user.uid)
+          .set({
+            username: socialAuthUser.user.displayName,
+            email: socialAuthUser.user.email,
+            roles: {},
+          });
+      })
+      .then(socialAuthUser => {
+        this.setState({ error: null });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+  render() {
+    const { error } = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <button type="submit">Sign In with Google</button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
 const SignInForm = compose(
   withRouter,
   withFirebase,
 )(SignInFormBase);
+
+const SignInGoogle = compose(
+  withRouter,
+  withFirebase,
+)(SignInGoogleBase);
+
 export default SignInPage;
-export { SignInForm };
+export { SignInForm, SignInGoogle };
