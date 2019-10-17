f4affe0c2ef743dd44d34ab33e9632860241c9df
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-16 18:59:20 -0700

    Simple firebase login setup

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,34 @@
+import app from 'firebase/app'
+import 'firebase/auth';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLsfpFpk2e2V-eDHuGfBbkAFuKHjzlJZI",
+    authDomain: "disney-ride-tracker.firebaseapp.com",
+    databaseURL: "https://disney-ride-tracker.firebaseio.com",
+    projectId: "disney-ride-tracker",
+    storageBucket: "disney-ride-tracker.appspot.com",
+    messagingSenderId: "536005158115",
+    appId: "1:536005158115:web:8d32ae833951cfe07fd1d6",
+    measurementId: "G-JY2KGR8MYF"
+}
+
+class Firebase {
+    constructor() {
+      app.initializeApp(firebaseConfig);
+
+      this.auth = app.auth();
+    }
+
+    doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password)
+
+    doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password)
+    
+    doSignOut = () => this.auth.signOut()
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email)
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker'
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>, 
+    document.getElementById('root')
+)
 
 registerServiceWorker()
