3f53374eb2e438f65d424ec2a974b0e82548c19c
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-19 18:17:38 -0700

    Import data

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AuthUserContext, withAuthorization } from '../Session';
 
+import SearchWeather from '../WeatherAPI'
+
 const WeatherPage = () => (
     <AuthUserContext.Consumer>
       {authUser => (
         <div>
           <h1>Weather Page</h1>
+          <h2>Data</h2>
         </div>
       )}
     </AuthUserContext.Consumer>
