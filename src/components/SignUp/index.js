c4ca39ab781e3a971153e2d4edbb5824534bdbd8
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-24 10:19:54 -0700

    Begin converting class to function

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
@@ -74,22 +74,24 @@ const INITIAL_STATE = {
   error: null,
 };
 
-class SignUpFormBase extends Component {
-  constructor(props) {
-    super(props);
+function SignUpFormBase(props) {
+  const [state, setState] = useState(...INITIAL_STATE)
 
-    this.state = { ...INITIAL_STATE };
-  }
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [passwordOne, setPasswordOne] = useState('')
+  const [passwordTwo, setPasswordTwo] = useState('')
+  const [error, setError] = useState(null)
+  const [isInvalid, setIsInvalid] = useState(true)
 
-  onSubmit = event => {
-    const { firstName, lastName, email, passwordOne } = this.state;
-
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
+  const onSubmit = event => {
+    props.firebase
+      .doCreateUserWithEmailAndPassword(state.email, state.passwordOne)
       .then(authUser => {
         // Create a user in your Firebase realtime database
         const logs = {'created': true}
-        return this.props.firebase
+        return props.firebase
           .user(authUser.user.uid)
           .set({
             firstName,
@@ -99,161 +101,149 @@ class SignUpFormBase extends Component {
           });
       })
       .then(authUser => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
+        props.history.push(ROUTES.HOME);
       })
       .catch(error => {
-        this.setState({ error });
+        setError(error);
       });
 
     event.preventDefault();
   }
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    const {
-      firstName,
-      lastName,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
-
-    const isInvalid =
-      passwordOne !== passwordTwo ||
+  const handleChange = event => {
+    setIsInvalid(passwordOne !== passwordTwo ||
       passwordOne === '' ||
       email === '' ||
       firstName === '' ||
-      lastName === '';
+      lastName === ''
+    )
 
-    const classes = {}
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-        </Typography>
-          <form className={classes.form} onSubmit={this.onSubmit} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  value={firstName}
-                  onChange={this.onChange}
-                  type="text"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                  value={lastName}
-                  onChange={this.onChange}
-                  type="text"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={this.onChange}
-                  type="text"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="passwordOne"
-                  label="Password"
-                  type="password"
-                  id="passwordOne"
-                  autoComplete="current-password"
-                  value={passwordOne}
-                  onChange={this.onChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="passwordTwo"
-                  label="Repeat Password"
-                  type="password"
-                  id="passwordTwo"
-                  autoComplete="current-password"
-                  value={passwordTwo}
-                  onChange={this.onChange}
-                  error={passwordOne !== passwordTwo}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
+    setState({...state, [event.target.name]: event.target.value})
+  };
+
+  const classes = useStyles()
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+      </Typography>
+        <form className={classes.form} onSubmit={onSubmit} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                value={firstName}
+                onChange={handleChange}
+                type="text"
+                autoFocus
+              />
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              disabled={isInvalid}
-            >
-              Sign Up
-          </Button>
-          <Snackbar 
-            open={error}
-            message={error ? error.message : null}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            autoHideDuration={6000} 
-           />
-            <Grid container justify="flex-end">
-              <Grid item>
-                <SignInLink />
-              </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={lastName}
+                onChange={handleChange}
+                type="text"
+              />
             </Grid>
-          </form>
-        </div>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
-    );
-  }
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={handleChange}
+                type="text"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="passwordOne"
+                label="Password"
+                type="password"
+                id="passwordOne"
+                autoComplete="current-password"
+                value={passwordOne}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="passwordTwo"
+                label="Repeat Password"
+                type="password"
+                id="passwordTwo"
+                autoComplete="current-password"
+                value={passwordTwo}
+                onChange={handleChange}
+                error={passwordOne !== passwordTwo}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={isInvalid}
+          >
+            Sign Up
+        </Button>
+        <Snackbar 
+          open={error}
+          message={error ? error.message : null}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          autoHideDuration={6000} 
+          />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <SignInLink />
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
 }
 
 const SignUpLink = () => (
