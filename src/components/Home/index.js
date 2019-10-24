5f6bc438823ddaa962a12688c7ee30767792dd85
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-10-24 00:34:20 -0700

    Add labels

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,14 @@ import './Home.css'
 import { withAuthorization } from '../Session';
 
 import SearchForm from '../SearchForm'
+import { Link } from '@material-ui/core';
 
 const HomePage = () => (
   <div>
+    <h1>Rides Today:</h1>
+    <h2>Rides Per Hour:</h2>
+    <h2>Park Ratio:</h2>
+    <Link>Previous Logs</Link>
     <SearchForm />
   </div>
 );
